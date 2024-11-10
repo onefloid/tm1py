0492b6c94b5f2a485f6a8cae29fc97a3c3d49eb9
onefloid <33193059+onefloid@users.noreply.github.com>
2024-11-10 13:08:16 +0100

    Test: Added skip pytest util function

--- Tests/Utils.py
+++ Tests/Utils.py
@@ -1,5 +1,5 @@
 import functools
-
+import pytest
 from TM1py.Utils.Utils import verify_version
 
 
@@ -40,6 +40,23 @@ def skip_if_version_lower_than(version):
 
     return wrap
 
+def skip_pytest_if_version_lower_than(required_version):
+    """ 
+    Checks whether TM1 version is lower than a certain version and skips the test 
+    if this is the case. This function is useful if a test needs a minimum required version.
+    """
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            # Get the fixture from the arguments
+            tm1 = kwargs.get("tm1")
+            if tm1 and not verify_version(required_version=required_version, version=tm1.version):
+                pytest.skip(
+                    f"Test requires TM1 server version >= {required_version}, but found {tm1.version}"
+                )
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
 
 def skip_if_version_higher_or_equal_than(version):
     """
