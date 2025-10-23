a579655113257a5f71fb4fd912be1b5448c924e1
onefloid <33193059+onefloid@users.noreply.github.com>
2025-10-23 20:39:38 +0200

    get rid of chore: comments

--- docs/gen_ref_stubs.py
+++ docs/gen_ref_stubs.py
@@ -76,18 +76,14 @@ api_map = {
 
 for category, entries in api_map.items():
     for name, import_path in entries.items():
-        # Kein "docs/" Prefix – direkt relativer Pfad
         filename = Path("reference") / category.lower() / f"{name.lower()}.md"
-
-        # Stub schreiben
         with mkdocs_gen_files.open(filename, "w") as f:
             f.write(f"<!-- {name}-->\n\n")
             f.write(f"::: {import_path}\n")
 
-        # Navigationseintrag
         rel_filename = Path(*filename.parts[1:])
         nav[category, name] = rel_filename
 
-# SUMMARY für mkdocs
+
 with mkdocs_gen_files.open("reference/SUMMARY.md", "w") as nav_file:
     nav_file.writelines(nav.build_literate_nav())
